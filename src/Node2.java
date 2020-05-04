c7888ccc9158f365498004164a6d4719bdcbbc60
MasonSchuckman <45863774+IhateKittenHat@users.noreply.github.com>
2020-05-04 19:46:32 -0400

    Testing

--- src/Node2.java
+++ src/Node2.java
@@ -1,25 +1,25 @@
 import java.util.ArrayList;
 
-public class Node {
-	Node parent;
-	ArrayList<Node>children;
-	public Node(Node p) {
-		children=new ArrayList<Node>(1);
+public class Node2 {
+	Node2 parent;
+	ArrayList<Node2>children;
+	public Node2(Node2 p) {
+		children=new ArrayList<Node2>(1);
 		parent=p;
 		if(p!=null) {
 			p.children.add(this);
 		}
 	}
 	
-	public int compareTo(Node n) {
+	public int compareTo(Node2 n) {
 		return 1;
 	}
 	
-	public int getChildren(Node b,int level) {
+	public int getChildren(Node2 b,int level) {
 		if(b.children.size()!=0)
 		System.out.format(" %d ",b.children.size());
 		int a=0; 
-		for(Node n:b.children) {
+		for(Node2 n:b.children) {
 			a+=n.children.size();
 			if(n.children.size()!=0)
 				System.out.print("  l: "+level);
